4777a236cb200e8a6b4626dec195aa42f1d7954a
Autumn Loveless <autumnloveless18@gmail.com>
2022-12-02 09:08:45 -0600

    day 2 part 2

--- 2022/day2-2.py
+++ 2022/day2-2.py
@@ -13,27 +13,35 @@ choice_value = {
     "C": 3  # sissor
 }
 
-choice_comparison = {
+fight_value = {
+    "X": 0,
+    "Y": 3,
+    "Z": 6
+}
+
+winning_move = {
     "A": "C", # rock beats sissor
     "B": "A", # paper beats rock
     "C": "B" # sissor beats paper
 }
+losing_move = { value: key for key,value in winning_move.items() } # flip the winning_move dict, for losers
 
-def fight(opponent_choice, my_choice) -> int:
-    if opponent_choice == my_choice:
-        return 3 # tie
-    if choice_comparison[my_choice] == opponent_choice:
-        return 6 # I win
+def determine_move(opponent_choice, result) -> str:
+    if result == "X":
+        return winning_move[opponent_choice] # I lose
+    if result == "Y":
+        return opponent_choice # I tie
     else:
-        return 0 # I lose
+        return losing_move[opponent_choice] # I win
     
 
 # Determine score
 score = 0
 for line in input:
-    [opponent_choice, my_choice_encrypted] = line.strip().split(" ") # process input
-    my_choice = choice_mapping[my_choice_encrypted] # convert to A,B,C
-    round_score = fight(opponent_choice, my_choice) + choice_value[my_choice] # get round score
+    [opponent_choice, result] = line.strip().split(" ") # process input
+    
+    my_choice = determine_move(opponent_choice, result) # determine move
+    round_score = choice_value[my_choice] + fight_value[result] # get round score
     score += round_score # add to score
 
 print(score)
