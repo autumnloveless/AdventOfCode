ec0c9a331d9ef579b832a79e20e9285da652faf4
Autumn Loveless <autumnloveless18@gmail.com>
2022-12-06 19:04:53 -0600

    day 6 part 2

---
+++ 2022/day6-2.py
@@ -0,0 +1,18 @@
+"""Advent of Code 2022 - Challenge 6 Part 2"""
+
+from typing import List
+
+# Get Input
+with open("inputs/day6.txt", 'r') as file:
+    input = [line.strip() for line in file.readlines()][0]
+
+def is_unique(substring):
+    set = { c for c in substring }
+    return len(set) == 14
+
+index = 3
+while index < len(input):
+    if is_unique(input[index-13:index+1]):
+        print(index+1)
+        break
+    index += 1
